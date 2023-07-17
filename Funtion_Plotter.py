39f4f02af4505b6b8cb8476b3192497523d71c21
mfawky <mfawky07@gmail.com>
2023-07-17 23:42:10 +0300

    importing libraries

---
+++ Funtion_Plotter.py
@@ -0,0 +1,11 @@
+import sys
+from PySide2.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QVBoxLayout,
+)
+import matplotlib.pyplot as plt
+import numpy as np
